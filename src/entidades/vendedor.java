04943390112ce53decdcaa7e04d6761ce84f134d
CintiaG.Sosa <cintiagSosa@users.noreply.github.com>
2023-10-03 12:30:55 -0300

    Create vendedor.java

---
+++ src/entidades/vendedor.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Raven
+ */
+public class vendedor {
+    
+    private int idVendedor;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private int telefono;
+    private boolean activo;
+
+    public vendedor() {
+    }
+
+    public vendedor(int idVendedor, String nombre, String apellido, int dni, int telefono, boolean activo) {
+        this.idVendedor = idVendedor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.activo = activo;
+    }
+
+    public vendedor(String nombre, String apellido, int dni, int telefono, boolean activo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.activo = activo;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return idVendedor + " " + nombre + " " + apellido + " " + dni + " " + telefono + " " + activo;
+    }
+    
+    
+    
+}
