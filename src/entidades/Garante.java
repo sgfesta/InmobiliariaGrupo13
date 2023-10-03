e3027dcbe6414e2723a8c028d187fbee421fbce1
MailenLG <profesorado.mai@gmail.com>
2023-10-03 12:10:56 -0300

    garante

--- src/entidades/Garante.java
+++ src/entidades/Garante.java
@@ -1,14 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidades;
 
-/**
- *
- * @author Usuario
- */
 public class Garante {
+
+    private int idGarante;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private String domicilio;
+    private int telefono;
+    private boolean activo;
+
+    public Garante() {
+    }
+
+    public Garante(String nombre, String apellido, int dni, String domicilio, int telefono, boolean activo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.activo = activo;
+    }
+
+    public Garante(int idGarante, String nombre, String apellido, int dni, String domicilio, int telefono, boolean activo) {
+        this.idGarante = idGarante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.activo = activo;
+    }
+
+    public int getIdGarante() {
+        return idGarante;
+    }
+
+    public void setIdGarante(int idGarante) {
+        this.idGarante = idGarante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Garante{" + "idGarante=" + idGarante + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", domicilio=" + domicilio + ", telefono=" + telefono + ", activo=" + activo + '}';
+    }
     
+    
+  
 }
