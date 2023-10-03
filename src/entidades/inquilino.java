eb948743889f356adadf4d3af01a7fc71378eefd
CintiaG.Sosa <cintiagSosa@users.noreply.github.com>
2023-10-03 12:11:48 -0300

    Update inquilino.java
    terminacion

--- src/entidades/inquilino.java
+++ src/entidades/inquilino.java
@@ -10,5 +10,70 @@ package entidades;
  * @author Raven
  */
 public class inquilino {
+
+    private int idInquilino;
+    private String name;
+    private String apellido;
+    private int dni;
+    private int cuit;
+    private String lugarTrabajo;
+    private boolean activo;
+
+    public inquilino() {
+    }
+
+    public inquilino(int idInquilino, String name, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
+        this.idInquilino = idInquilino;
+        this.name = name;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.cuit = cuit;
+        this.lugarTrabajo = lugarTrabajo;
+        this.activo = activo;
+    }
+
+    public inquilino(String name, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
+        this.name = name;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.cuit = cuit;
+        this.lugarTrabajo = lugarTrabajo;
+        this.activo = activo;
+    }
+
+    public int getIdInquilino() {
+        return idInquilino;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public String getLugarTrabajo() {
+        return lugarTrabajo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    @Override
+    public String toString() {
+        return  idInquilino + " " + name + " " + apellido + " " + dni + " " + cuit + " " + lugarTrabajo + " " + activo;
+    }
     
+    
+
 }
