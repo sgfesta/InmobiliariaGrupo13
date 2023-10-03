8632cba97dfab5a219ce0a039c16e455d40e972a
Matiaschico3 <matiaschico3@gmail.com>
2023-10-03 12:53:19 -0300

    toString de inspector y clases

---
+++ src/entidades/Zona.java
@@ -0,0 +1,66 @@
+
+package entidades;
+
+
+public class Zona {
+    private int idZona;
+    private String provincia;
+    private String ciudad;
+    private String barrio;
+
+    public Zona(int idZona, String provincia, String ciudad, String barrio) {
+        this.idZona = idZona;
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+        this.barrio = barrio;
+    }
+
+    public Zona(String provincia, String ciudad, String barrio) {
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+        this.barrio = barrio;
+    }
+
+    public Zona() {
+    }
+
+    public int getIdZona() {
+        return idZona;
+    }
+
+    public void setIdZona(int idZona) {
+        this.idZona = idZona;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getBarrio() {
+        return barrio;
+    }
+
+    public void setBarrio(String barrio) {
+        this.barrio = barrio;
+    }
+
+    @Override
+    public String toString() {
+        return idZona + " " + provincia + " " + ciudad + " " + barrio;
+    }
+    
+    
+
+}
