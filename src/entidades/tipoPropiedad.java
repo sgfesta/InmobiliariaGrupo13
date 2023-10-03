8ec9b5ace36fd06ed4847f199accdc40a4b3ec8f
Sergio Festa <71560110+sgfesta@users.noreply.github.com>
2023-10-03 12:40:16 -0300

    tipo de propiedad

--- src/entidades/tipoPropiedad.java
+++ src/entidades/tipoPropiedad.java
@@ -11,4 +11,37 @@ package entidades;
  */
 public class tipoPropiedad {
     
+    private int idTipo;
+    private String nombre ;
+
+    public tipoPropiedad(int idTipo, String nombre) {
+        this.idTipo = idTipo;
+        this.nombre = nombre;
+    }
+
+    public tipoPropiedad() {
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "tipoPropiedad{" + "idTipo=" + idTipo + ", nombre=" + nombre + '}';
+    }
+    
+    
 }
