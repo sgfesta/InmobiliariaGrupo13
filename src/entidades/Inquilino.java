3a6a6fdd83100e8d69f9305c68a0f5925d646637
MailenLG <profesorado.mai@gmail.com>
2023-10-04 10:57:02 -0300

    inquilino

--- src/entidades/Inquilino.java
+++ src/entidades/Inquilino.java
@@ -7,7 +7,7 @@ public class Inquilino {
     //Declaro las variables
 
     private int idInquilino;
-    private String name;
+    private String nombre;
     private String apellido;
     private int dni;
     private int cuit;
@@ -19,9 +19,9 @@ public class Inquilino {
     public Inquilino() {
     }
 
-    public Inquilino(int idInquilino, String name, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
+    public Inquilino(int idInquilino, String nombre, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
         this.idInquilino = idInquilino;
-        this.name = name;
+        this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.cuit = cuit;
@@ -29,8 +29,8 @@ public class Inquilino {
         this.activo = activo;
     }
 
-    public Inquilino(String name, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
-        this.name = name;
+    public Inquilino(String nombre, String apellido, int dni, int cuit, String lugarTrabajo, boolean activo) {
+        this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.cuit = cuit;
@@ -44,35 +44,61 @@ public class Inquilino {
         return idInquilino;
     }
 
-    public String getName() {
-        return name;
+    public void setIdInquilino(int idInquilino) {
+        this.idInquilino = idInquilino;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getApellido() {
         return apellido;
     }
 
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public int getDni() {
         return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
     }
 
     public int getCuit() {
         return cuit;
     }
 
+    public void setCuit(int cuit) {
+        this.cuit = cuit;
+    }
+
     public String getLugarTrabajo() {
         return lugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String lugarTrabajo) {
+        this.lugarTrabajo = lugarTrabajo;
     }
 
     public boolean isActivo() {
         return activo;
     }
 
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
     @Override
     public String toString() {
-        return  idInquilino + " " + name + " " + apellido + " " + dni + " " + cuit + " " + lugarTrabajo + " " + activo;
+        return  idInquilino + " " + nombre + " " + apellido + " " + dni + " " + cuit + " " + lugarTrabajo + " " + activo;
     }
     
-    
-
 }
