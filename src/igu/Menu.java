80056185648847b56d8d9bf3299fa992de84a2f7
Sergio Festa <71560110+sgfesta@users.noreply.github.com>
2023-10-05 12:26:54 -0300

    vistas

--- src/igu/Menu.java
+++ src/igu/Menu.java
@@ -33,11 +33,11 @@ public class Menu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 977, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 663, Short.MAX_VALUE)
         );
 
         pack();
