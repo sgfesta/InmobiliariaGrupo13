27a4d72b1e1feca23af9bfae86986d791816ca53
Matiaschico3 <matiaschico3@gmail.com>
2023-10-05 12:50:11 -0300

    pruebas de algunas cosas en el main

--- src/logica/main.java
+++ src/logica/main.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package logica;
 
-/**
- *
- * @author Pirrupi
- */
+import entidades.Garante;
+import entidades.Inquilino;
+import entidades.Inspector;
+import persistencia.GaranteData;
+import persistencia.InquilinoData;
+import persistencia.InspectorData;
+
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//        Inspector inspec = new Inspector("D-51657", "Jose", "Ramirez", 638, 156784576, "Casita 123", true);
+//        InspectorData ins = new InspectorData();
+//        ins.guardarInspector(inspec);
+        
+//        Inquilino i1 = new Inquilino("Pedro", "Perez", 111, 1654, "Fabrica", true);
+//        InquilinoData In = new InquilinoData();
+//        In.guardarInquilino(i1);
     
+    
+//    Garante garante = new Garante("Luis", "Medina", 987, "No tiene casa", 168496874, true);
+//    GaranteData g1 = new GaranteData();
+//    g1.guardarGarante(garante);
+    
+}
 }
