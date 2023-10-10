70ed7b79398845c47fdb999a67b1648e0b0f2e4c
MailenLG <profesorado.mai@gmail.com>
2023-10-10 12:51:15 -0300

    login

--- src/igu/MenuTest.java
+++ src/igu/MenuTest.java
@@ -29,6 +29,7 @@ public class MenuTest extends javax.swing.JFrame {
         paraListadosGarante();
         paraListadosInquilinos();
         paraListadosInspector();
+        bg.setVisible(false);
 
     }
 
@@ -51,6 +52,16 @@ public class MenuTest extends javax.swing.JFrame {
         jPCardFondo = new javax.swing.JPanel();
         logo1 = new javax.swing.JLabel();
         jPLogin1 = new javax.swing.JPanel();
+        bg = new javax.swing.JPanel();
+        logo2 = new javax.swing.JLabel();
+        userTxt = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        passTxt = new javax.swing.JPasswordField();
+        jSeparator2 = new javax.swing.JSeparator();
+        loginBtn = new javax.swing.JPanel();
+        loginBtnTxt = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
         jPPropiedades = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -446,15 +457,111 @@ public class MenuTest extends javax.swing.JFrame {
 
         jPLogin1.setBackground(new java.awt.Color(255, 51, 153));
 
+        bg.setBackground(new java.awt.Color(51, 51, 51));
+        bg.setEnabled(false);
+        bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        logo2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        logo2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/logoceleste.png"))); // NOI18N
+        bg.add(logo2, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 90, 200, 220));
+
+        userTxt.setBackground(new java.awt.Color(51, 51, 51));
+        userTxt.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        userTxt.setForeground(new java.awt.Color(255, 255, 255));
+        userTxt.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        userTxt.setText("Ingrese nombre de usuario");
+        userTxt.setBorder(null);
+        userTxt.setDisabledTextColor(new java.awt.Color(255, 255, 255));
+        userTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                userTxtMousePressed(evt);
+            }
+        });
+        userTxt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userTxtActionPerformed(evt);
+            }
+        });
+        bg.add(userTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 370, 290, 30));
+
+        jSeparator1.setBackground(new java.awt.Color(255, 255, 255));
+        jSeparator1.setForeground(new java.awt.Color(255, 255, 255));
+        bg.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 400, 290, 20));
+
+        passTxt.setBackground(new java.awt.Color(51, 51, 51));
+        passTxt.setForeground(new java.awt.Color(255, 255, 255));
+        passTxt.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        passTxt.setText("********");
+        passTxt.setBorder(null);
+        passTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                passTxtMousePressed(evt);
+            }
+        });
+        bg.add(passTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 430, 290, 30));
+
+        jSeparator2.setBackground(new java.awt.Color(255, 255, 255));
+        jSeparator2.setForeground(new java.awt.Color(255, 255, 255));
+        bg.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 460, 290, 20));
+
+        loginBtn.setBackground(new java.awt.Color(51, 51, 51));
+        loginBtn.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)), null));
+
+        loginBtnTxt.setBackground(new java.awt.Color(255, 255, 255));
+        loginBtnTxt.setFont(new java.awt.Font("Roboto Condensed", 1, 14)); // NOI18N
+        loginBtnTxt.setForeground(new java.awt.Color(255, 255, 255));
+        loginBtnTxt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        loginBtnTxt.setText("INGRESAR");
+        loginBtnTxt.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        loginBtnTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                loginBtnTxtMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                loginBtnTxtMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                loginBtnTxtMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout loginBtnLayout = new javax.swing.GroupLayout(loginBtn);
+        loginBtn.setLayout(loginBtnLayout);
+        loginBtnLayout.setHorizontalGroup(
+            loginBtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(loginBtnLayout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(loginBtnTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+        loginBtnLayout.setVerticalGroup(
+            loginBtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginBtnLayout.createSequentialGroup()
+                .addComponent(loginBtnTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        bg.add(loginBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 500, 130, 40));
+
+        jLabel4.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("GRUPO 13");
+        bg.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 310, 90, -1));
+
+        jLabel7.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("INMOBILIARIA");
+        bg.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 330, 130, -1));
+
         javax.swing.GroupLayout jPLogin1Layout = new javax.swing.GroupLayout(jPLogin1);
         jPLogin1.setLayout(jPLogin1Layout);
         jPLogin1Layout.setHorizontalGroup(
             jPLogin1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 748, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 760, Short.MAX_VALUE)
         );
         jPLogin1Layout.setVerticalGroup(
             jPLogin1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 635, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 635, Short.MAX_VALUE)
         );
 
         Fondo.add(jPLogin1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 65, -1, -1));
@@ -2214,6 +2321,7 @@ public class MenuTest extends javax.swing.JFrame {
         jPCardListados.setVisible(false);
         jPCardFondo.setVisible(false);
         jPLogin.setVisible(false);
+        bg.setVisible(false);
 
     }//GEN-LAST:event_jLPropiedadesMouseClicked
 
@@ -2225,6 +2333,7 @@ public class MenuTest extends javax.swing.JFrame {
         jPCardListados.setVisible(false);
         jPCardFondo.setVisible(false);
         jPLogin.setVisible(false);
+        bg.setVisible(false);
 
     }//GEN-LAST:event_jLPropietariosMouseClicked
 
@@ -2236,6 +2345,7 @@ public class MenuTest extends javax.swing.JFrame {
         jPCardAlquileres.setVisible(true);
         jPCardListados.setVisible(false);
         jPLogin.setVisible(false);
+        bg.setVisible(false);
 
 
     }//GEN-LAST:event_jLAlquileresMouseClicked
@@ -2248,6 +2358,7 @@ public class MenuTest extends javax.swing.JFrame {
         jPCardListados.setVisible(true);
         jPCardFondo.setVisible(false);
         jPLogin.setVisible(false);
+        bg.setVisible(false);
         
         if(jPInquilinos.isVisible()==true && jPGarante.isVisible()==false && jPInspector.isVisible()==false){
         paraListadosInquilinosTrue();
@@ -2266,7 +2377,7 @@ public class MenuTest extends javax.swing.JFrame {
     }//GEN-LAST:event_jListadosMouseClicked
 
     private void jLLoginMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLLoginMouseClicked
-
+        bg.setVisible(true);
         jPLogin1.setVisible(true);
         jPCardPropietarios.setVisible(false);
         jPPropiedades.setVisible(false);
@@ -2461,6 +2572,45 @@ public class MenuTest extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jTabbedPane1StateChanged
 
+    private void userTxtMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_userTxtMousePressed
+        if (userTxt.getText().equals("Ingrese nombre de usuario")) {
+            userTxt.setText("");
+            userTxt.setForeground(Color.white);
+        }
+        if (String.valueOf(passTxt.getPassword()).isEmpty()) {
+            passTxt.setText("********");
+
+            passTxt.setForeground(Color.white);
+        }
+    }//GEN-LAST:event_userTxtMousePressed
+
+    private void userTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userTxtActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_userTxtActionPerformed
+
+    private void passTxtMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_passTxtMousePressed
+        if (String.valueOf(passTxt.getPassword()).equals("********")) {
+            passTxt.setText("");
+            passTxt.setForeground(Color.white);
+        }
+        if (userTxt.getText().isEmpty()) {
+            userTxt.setText("Ingrese nombre de usuaio");
+            userTxt.setForeground(Color.white);
+        }
+    }//GEN-LAST:event_passTxtMousePressed
+
+    private void loginBtnTxtMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnTxtMouseClicked
+        javax.swing.JOptionPane.showMessageDialog(this, "Intento de login con los datos:\nUsuario: " + userTxt.getText() + "\nContraseña: " + String.valueOf(passTxt.getPassword()), "LOGIN", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_loginBtnTxtMouseClicked
+
+    private void loginBtnTxtMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnTxtMouseEntered
+        loginBtn.setBackground(new Color(102, 102, 102));
+    }//GEN-LAST:event_loginBtnTxtMouseEntered
+
+    private void loginBtnTxtMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnTxtMouseExited
+        loginBtn.setBackground(new Color(51, 51, 51));
+    }//GEN-LAST:event_loginBtnTxtMouseExited
+
     /**
      * @param args the command line arguments
      */
@@ -2499,6 +2649,7 @@ public class MenuTest extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Fondo;
+    private javax.swing.JPanel bg;
     private javax.swing.JLabel exitTxt;
     private javax.swing.JPanel header;
     private javax.swing.JButton jBEliminar;
@@ -2599,6 +2750,7 @@ public class MenuTest extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel37;
     private javax.swing.JLabel jLabel38;
     private javax.swing.JLabel jLabel39;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel40;
     private javax.swing.JLabel jLabel41;
     private javax.swing.JLabel jLabel42;
@@ -2607,6 +2759,7 @@ public class MenuTest extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel45;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JLabel jListados;
@@ -2627,6 +2780,8 @@ public class MenuTest extends javax.swing.JFrame {
     private javax.swing.JPanel jPMenuLateral;
     private javax.swing.JPanel jPPropiedades;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JTextField jTActivo;
     private javax.swing.JTextField jTActivoGarante;
     private javax.swing.JTextField jTActivoInquilinos;
@@ -2676,8 +2831,13 @@ public class MenuTest extends javax.swing.JFrame {
     private javax.swing.JTextField jTVigencia;
     private javax.swing.JTextField jTZona;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JPanel loginBtn;
+    private javax.swing.JLabel loginBtnTxt;
     private javax.swing.JLabel logo;
     private javax.swing.JLabel logo1;
+    private javax.swing.JLabel logo2;
+    private javax.swing.JPasswordField passTxt;
+    private javax.swing.JTextField userTxt;
     // End of variables declaration//GEN-END:variables
 
     public void paraPropiedades() {
