696bd3e67c9914b63c7229a0d757d8b96a95ab5c
Matiaschico3 <matiaschico3@gmail.com>
2023-10-19 11:52:20 -0300

    Abrir programa desde el Main

--- src/logica/main.java
+++ src/logica/main.java
@@ -8,53 +8,60 @@ import entidades.Propietario;
 import entidades.TipoPropiedad;
 import entidades.Vendedor;
 import entidades.Zona;
+import igu.MenuPrincipal;
 import persistencia.ContratoData;
 
 public class main {
 
 
     public static void main(String[] args) {
+             java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipal().setVisible(true);
+            }
+        });
+    }
+}
+        
         //Inspector inspec = new Inspector(1,"D-51657", "Jose", "Ramirez", 638, 156784576, "Casita 123", true);
 //        InspectorData ins = new InspectorData();
 //        ins.guardarInspector(inspec);
 //        
-        Inquilino i1 = new Inquilino(1,"Pedro", "Perez", "111", "1654", "Fabrica", true);
-//        InquilinoData In = new InquilinoData();
-//        In.guardarInquilino(i1);
-//    
-//    
-    Garante garante = new Garante(1,"Luis", "Medina", 987, "No tiene casa", 168496874, true);
-//    GaranteData g1 = new GaranteData();
-//    g1.guardarGarante(garante);
-//
-      Vendedor vendedor = new Vendedor(1,"Fulanito", "Cosmo", "64841", "26987546", true);
-      
-//      VendedorData vd = new VendedorData();
-//      vd.guardarVendedor(vendedor);
-//
-      Estado estado = new Estado(1,"Bueno", true);
-//      EstadoData e1 = new EstadoData();
-//      e1.darAlta(estado);
+//        Inquilino i1 = new Inquilino(1,"Pedro", "Perez", "111", "1654", "Fabrica", true);
+////        InquilinoData In = new InquilinoData();
+////        In.guardarInquilino(i1);
+////    
+////    
+//    Garante garante = new Garante(1,"Luis", "Medina", 987, "No tiene casa", 168496874, true);
+////    GaranteData g1 = new GaranteData();
+////    g1.guardarGarante(garante);
+////
+//      Vendedor vendedor = new Vendedor(1,"Fulanito", "Cosmo", "64841", "26987546", true);
 //      
-      Zona zona = new Zona(1,"Entre Rios", "Parana", "Puerto", true);
-//      ZonaData zd = new ZonaData();
-//      zd.guardarZona(zona);
+////      VendedorData vd = new VendedorData();
+////      vd.guardarVendedor(vendedor);
+////
+//      Estado estado = new Estado(1,"Bueno", true);
+////      EstadoData e1 = new EstadoData();
+////      e1.darAlta(estado);
+////      
+//      Zona zona = new Zona(1,"Entre Rios", "Parana", "Puerto", true);
+////      ZonaData zd = new ZonaData();
+////      zd.guardarZona(zona);
+////      
+////      
+//      TipoPropiedad tipo = new TipoPropiedad(1,"Casa", true);
+////      TipoPropiedadData tpd = new TipoPropiedadData();
+////      tpd.guardarTipoPropiedad(tipo);
 //      
+//      Propietario propietario = new Propietario(1,"Luciana", "Juarez", "712", "2348458", "Su casa", true);
+////      PropietarioData p1 = new PropietarioData();
+////      p1.guardarPropietario(propietario);
 //      
-      TipoPropiedad tipo = new TipoPropiedad(1,"Casa", true);
-//      TipoPropiedadData tpd = new TipoPropiedadData();
-//      tpd.guardarTipoPropiedad(tipo);
-      
-      Propietario propietario = new Propietario(1,"Luciana", "Juarez", "712", "2348458", "Su casa", true);
-//      PropietarioData p1 = new PropietarioData();
-//      p1.guardarPropietario(propietario);
-      
-//      Propiedad propi = new Propiedad(1, propietario, "Belgrano", 23, tipo, 1800, 150000, 0, inspec, zona, estado, "Departamento nuevo, a estrenar, bla bla bla", true, true);
-//      PropiedadData pd = new PropiedadData();
-//      pd.agregarPropiedad(propi);
-      
-//      Contrato contract = new Contrato(propietario, propi, i1, garante, vendedor, LocalDate.of(2018, Month.MARCH, 05), LocalDate.of(2026, Month.MARCH, 05), LocalDate.of(2018, Month.MARCH, 02), true, true);
-      ContratoData cd = new ContratoData();
-//      cd.guardarContrato(contract);
-}
-}
+////      Propiedad propi = new Propiedad(1, propietario, "Belgrano", 23, tipo, 1800, 150000, 0, inspec, zona, estado, "Departamento nuevo, a estrenar, bla bla bla", true, true);
+////      PropiedadData pd = new PropiedadData();
+////      pd.agregarPropiedad(propi);
+//      
+////      Contrato contract = new Contrato(propietario, propi, i1, garante, vendedor, LocalDate.of(2018, Month.MARCH, 05), LocalDate.of(2026, Month.MARCH, 05), LocalDate.of(2018, Month.MARCH, 02), true, true);
+//      ContratoData cd = new ContratoData();
+////      cd.guardarContrato(contract);
